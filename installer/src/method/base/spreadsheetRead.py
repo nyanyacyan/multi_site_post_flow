2ca7e417fb665874bf5582f0c1e2ca243b9587a1
nyanyacyan <>
2024-12-28 12:17:07 +0900

    GUIで使うようのWorksheetのリストを返すMethodを作成

--- installer/src/method/base/spreadsheetRead.py
+++ installer/src/method/base/spreadsheetRead.py
@@ -169,6 +169,27 @@ class GetDataGSSAPI:
 
         return df
 
+
+    # ----------------------------------------------------------------------------------
+    # スプシにあるWorksheetのリストを返す
+    # GUIに返す
+
+    def _get_all_worksheet(self, gss_info: Dict):
+        client = self.client(jsonKeyName=gss_info["jsonKeyName"])
+
+        # 対象のスプシを開く
+        spreadsheet = client.open_by_key(gss_info["spreadsheetId"])
+
+        # すべてのWorksheetオブジェクトを抽出
+        all_worksheet = spreadsheet.worksheets()
+        self.logger.debug(f'all_worksheet: {all_worksheet}')
+
+        # すべてのワークシートのリスト
+        worksheet_title_list = [ws.title for ws in all_worksheet]
+        self.logger.debug(f"ワークシート全データ: {worksheet_title_list}")
+
+        return worksheet_title_list
+
     # ----------------------------------------------------------------------------------
     # スプシの認証プロパティ
 
